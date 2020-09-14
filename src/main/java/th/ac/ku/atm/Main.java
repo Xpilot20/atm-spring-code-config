8e3fb6111901bd69db2f02ad5b7440c472a0a74d
Xpilot20 <peatsaowahong20@gmail.com>
2020-09-14 20:48:12 +0700

    final commit

--- src/main/java/th/ac/ku/atm/Main.java
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -3,16 +3,17 @@ package th.ac.ku.atm;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(ATMConfig.class);
         ATM atm = context.getBean(ATM.class);
 
-        String name = atm.validateCustomer(2,2345);
+        String name = atm.validateCustomer(1,1234);
         System.out.println( name + " has " + atm.getBalance() );
         atm.withdraw(200);
         System.out.println( name + " has " + atm.getBalance() );
-
     }
 }
